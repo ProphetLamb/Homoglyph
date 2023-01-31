2d5c978544731c4b325a44eb18ad62896af44320
Prophet Lamb <prophet.lamb@gmail.com>
2023-01-31 18:58:59 +0100

    remove altcover filter

--- test.ps1
+++ test.ps1
@@ -1,4 +1,4 @@
-dotnet test --no-restore --no-build --test-adapter-path:. --logger:Appveyor /p:AltCover='true' /p:AltCoverAssemblyExcludeFilter='^(?!(src)).*$'
+dotnet test --no-restore --no-build --test-adapter-path:. --logger:Appveyor /p:AltCover='true'
 
 $commitID = & git rev-parse HEAD
 $commitBranch = & git rev-parse --abbrev-ref HEAD
