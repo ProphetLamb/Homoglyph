3e652eb79bbc132d7218afb6c3b542bcdce67604
Prophet Lamb <prophet.lamb@gmail.com>
2023-01-31 21:53:53 +0100

    Refactor test script

--- test.ps1
+++ test.ps1
@@ -1,35 +1,38 @@
 $dir = (Get-Item $pwd).FullName + "\src"
-dotnet test --no-restore --no-build --test-adapter-path:$dir --logger:Appveyor /p:AltCover='true' /p:AltCoverAssemblyExcludeFilter='^(?!Homoglyph).*$|Test.*$'
+$moduleExclude = '^(?!Homoglyph).*$|Test.*$'
+dotnet test --no-restore --no-build --test-adapter-path:$dir --logger:Appveyor /p:AltCover='true' /p:AltCoverAssemblyExcludeFilter=$moduleExclude
+
+$commitID = & git rev-parse HEAD
+$commitBranch = & git rev-parse --abbrev-ref HEAD
 
 foreach ($coverage in Get-ChildItem -Recurse -File coverage*.xml) {
     # postprocess the coverage files
     $coverage_xml = [xml](Get-Content $coverage.FullName)
     $coverage_xml.CoverageSession.Modules.Module | % {
-        if ($_.ModuleName -match '^(?!Homoglyph).*$|Test.*$') {
+        if ($_.ModuleName -match $moduleExclude) {
+            # remove excluded modules
             $_.ParentNode.RemoveChild($_)
         }
         if ($_.skippedDueTo -eq 'Filter') {
+            # remove filtered modules
             $_.ParentNode.RemoveChild($_)
         }
 
         $_.Files.File | % {
             if ($_.fullPath -match '^[\\/]_[\\/]') {
+                # trim /_/ prefix for project directory
                 $_.fullPath = $_.fullPath -replace '^[\\/]_[\\/]', ''
             }
             else {
+                # remove files outside of the project directory
                 $_.ParentNode.RemoveChild($_)
             }
         }
-    }
-    # 3. trim $pwd from /CoverageSession/Modules/Module/ModulePath
-    $coverage_xml.CoverageSession.Modules.Module | % {
+        # make absolute paths relative
         $_.ModulePath = $_.ModulePath -replace [regex]::escape($dir), ''
     }
     # wirte the changes back to the file
     $coverage_xml.Save($coverage.FullName)
-
-    $commitID = & git rev-parse HEAD
-    $commitBranch = & git rev-parse --abbrev-ref HEAD
 
     & csmacnz.Coveralls --useRelativePaths --basePath $dir --opencover -i $coverage --commitId $commitID --commitBranch $commitBranch
 }
