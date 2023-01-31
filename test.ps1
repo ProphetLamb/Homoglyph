3e3561489b7ccbb4510850908b15ae8cbbf5e97d
Prophet Lamb <prophet.lamb@gmail.com>
2023-01-31 17:51:48 +0100

    Simplify filter

--- test.ps1
+++ test.ps1
@@ -1,4 +1,4 @@
-dotnet test --no-restore --no-build --test-adapter-path:. --logger:Appveyor /p:AltCover="true" /p:AltCoverAssemblyFilter='^Homoglyph(?!.*Test)'
+dotnet test --no-restore --no-build --test-adapter-path:. --logger:Appveyor /p:AltCover="true" /p:AltCoverAssemblyExcludeFilter=NUnit|*Test
 
 $commitID = & git rev-parse HEAD
 $commitBranch = & git rev-parse --abbrev-ref HEAD
